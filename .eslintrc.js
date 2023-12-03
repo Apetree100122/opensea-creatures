d616105d4c46097c93deb5e22ebda761da669aa0
Alexander Petree <apetree1001@email.phoenix.edu>
2023-12-02 22:54:43 -0800

    Update .eslintrc.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,14 +1,1 @@
-module.exports = {
-  env: {
-    browser: true,
-    commonjs: true,
-    es6: true,
-  },
-  globals: {
-    Atomics: "readonly",
-    SharedArrayBuffer: "readonly",
-  },
-  parserOptions: {
-    ecmaVersion: 2018,
-  },
-};
+module.exports = {  env: {  browser: true, commonjs: true,   es6: true, }, globals: { Atomics: "readonly",SharedArrayBuffer: "readonly", }, parserOptions: { ecmaVersion: 2018,},};
